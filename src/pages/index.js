3977a44aa963cef8fb5e0cc5fc17f917088fef48
Daniele Marangoni <danielemarangoni@Mac.homenet.telecomitalia.it>
2026-03-13 04:39:31 +0100

    Ripristinata grafica originale con globals.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import '../styles/globals.css'; // Carica il tuo CSS bellissimo
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { saveAs } from 'file-saver';
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
