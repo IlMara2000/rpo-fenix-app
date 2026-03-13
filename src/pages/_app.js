3a76e42e1a129cbe39227da8973ee7dd9a11c3c8
Daniele Marangoni <danielemarangoni@Mac.homenet.telecomitalia.it>
2026-03-13 04:47:19 +0100

    Spostato CSS globale in _app.js come richiesto da Nextjs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,5 @@
+import '../styles/globals.css'; // Il CSS va solo qui!
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { saveAs } from 'file-saver';
